4799f147507448526da5fe8403789f767522b682
PelleyZack <70712792+PelleyZack@users.noreply.github.com>
2022-03-08 10:59:47 -0500

    Update task2.php
    Updated to make the code more readable, added comments, changed it so it would make new book insertions for each type of book offered and the price  changes according to the type of book with hardcover being more expensive and e_book being least expensive.

--- task2.php
+++ task2.php
@@ -4,7 +4,8 @@
   $auid = $_POST['author_id'];
   $btitle = $_POST['book_title'];
   $bgenre = $_POST['book_genre'];
-  $btype = $_POST['book_type']
+  $isbn = $_POST['isbn'];
+  $btype = $_POST['book_type'];
 ?>
 <html>
 <head>
@@ -26,86 +27,55 @@
 	echo "<br>";
 	$condition = '';
 	$money = 19.99;
-	if(!empty($_POST['book_type'])){
-		foreach($_POST['book_type'] as $type){
-			echo $type;
-			$condition  .= $type . ',';
-			$money += 5.00;
-			echo "<br>";
-		}
-	}
-	echo date("Y/m/d");
-	echo "<br>";
-	echo $condition;
-	echo "<br>";
-	echo $money;
-	echo "<br>";
+	$year = intval(date('Y'));
 	
-	/*Three layers to check, first makes sure the publisher exists
-	then checks that the author does, then it checks if the title
-	is allready in use, if they pass all the checks then it inserts 
-	the new book into the book table, and their relationships with 
-	wrote and published is inserted as well*/
-	
+	//Checking if publisher exists (needs to exsist in database to proceed)
 	$qpid = "SELECT * FROM publisher where publisher_id = '" .mysqli_real_escape_string($myconnection, $pid) . "'";
 	$result = mysqli_query($myconnection, $qpid);
 	if(mysqli_num_rows($result) == 1){
-		echo "pass";
-		echo "<br>";
+
+
+		//Checking if author exists (needs to exsist in database to proceed)
 		$qauid = "SELECT * FROM author where author_id = '" .mysqli_real_escape_string($myconnection, $auid) . "'";
 		$result = mysqli_query($myconnection, $qauid);
 		if(mysqli_num_rows($result) == 1){
-			echo "pass";
-			echo "<br>";
-		
+
+			//Checking if the book title is allready in use (book of the same title cannot exsist in database to proceed)
 			$qtitle = "SELECT * FROM book where title = '" .mysqli_real_escape_string($myconnection, $btitle) . "'";
 			$result = mysqli_query($myconnection, $qtitle);
 			if(mysqli_num_rows($result) == 0){
-				echo "pass";
-				echo "<br>";
 				
-				//This loop generates an id number for the book and makes sure it is not in use
-				do{
-				$bid = strval(rand(0,9999));
-				$qtestid = "SELECT * FROM book where book_id = '$bid'";
-				$result = mysqli_query($myconnection, $qtestid);
-				}while(mysqli_num_rows($result) != 0);
-				echo $bid;
-				echo "<br>";
-				$year = intval(date('Y'));
-				
-				$qfinal = "INSERT INTO book (book_id,year,genre,title,isbn,book_condition,price,book_type, total_rating) values ('$bid','$year','$bgenre','$btitle','13516', 'new', '$money','$condition', '0.0')";
-				if(mysqli_query($myconnection, $qfinal)){
-					echo "Inserted new book";
-					echo "<br>";
+				//Iterating through book types
+				if(!empty($_POST['book_type'])){
+					foreach($_POST['book_type'] as $type){
+						
+						//Checking book types and setting price accordingly
+						if($type == 'e_book'){$money = 14.99;}
+						if($type == 'hard_cover'){$money = 24.99;}
+						if($type == 'paper_back'){$money = 19.99;}
+						
+						//generating random book id
+						do{
+						$bid = strval(rand(0,9999));
+						$qtestid = "SELECT * FROM book where book_id = '$bid'";
+						$result = mysqli_query($myconnection, $qtestid);
+						}while(mysqli_num_rows($result) != 0);
+									
+						//Finally inserting into book, wrote, and publish tables
+						$qfinal = "INSERT INTO book (book_id,year,genre,title,isbn,book_condition,price,book_type, total_rating) values ('$bid','$year','$bgenre','$btitle','$isbn', 'new', '$money','$type', '0.0')";
+						if(mysqli_query($myconnection, $qfinal)){echo "Inserted new book<br>";}
+						$qfinal1 = "insert into published (publisher_id,book_id) values ('$pid','$bid')";
+						if(mysqli_query($myconnection, $qfinal1)){echo "Inserted new published<br>";}
+						$qfinal2 = "insert into wrote (author_id,book_id) values ('$auid','$bid')";
+						if(mysqli_query($myconnection, $qfinal2)){echo "Inserted new wrote<br>";}
+					}
 				}
-				$qfinal1 = "insert into published (publisher_id,book_id) values ('$pid','$bid')";
-				if(mysqli_query($myconnection, $qfinal1)){
-					echo "Inserted new published";
-					echo "<br>";
-				}
-				$qfinal2 = "insert into wrote (author_id,book_id) values ('$auid','$bid')";
-				if(mysqli_query($myconnection, $qfinal2)){
-					echo "Inserted new wrote";
-					echo "<br>";
-				}
-
 			}
-			else {
-				echo "fail";
-				echo "<br>";
-			}
-		
+else {echo "fail<br>";}
 		}
-		else{
-			echo "fail";
-			echo "<br>";
-		}
+else{echo "fail<br>";}
 	}
-	else{
-		echo "fail";
-		echo "<br>";
-	}
+else{echo "fail<br>";}
 ?>
 </body>
 </html>
