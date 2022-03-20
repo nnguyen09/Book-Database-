9ad8916c6e48b1bd878f448b655c2b5d5ff9a6ac
Ngan Nguyen <nnguyen09@northshore.edu>
2022-03-19 22:33:09 -0400

    03/19

--- cart.php
+++ cart.php
@@ -1,61 +1,47 @@
 <?php
-
-  $email =$_SESSION['email']; 
-  echo $email;
+session_start();
+  $email =$_SESSION['email'];  
 
   // create short variable names
   //$get_current_list_of_books = "select list_of_books from cart where email ='$email'";
   //$list_of_books = '';
-  foreach($_POST as $book_quantity => $value){
-    if($value != 0){
+
+   
+  $myconnection = mysqli_connect('localhost', 'root', '') 
+  or die ('Could not connect: ' . mysql_error());
+
+  $mydb = mysqli_select_db ($myconnection, 'bookstore') or die ('Could not select database');
+  $cartq="select cart_id from cart where user_email = '$email'";
+  $rcart = mysqli_query($myconnection, $cartq) or die ('Query failed: ' . mysql_error());
+//grabbing cart id
+while ($row = mysqli_fetch_array ($rcart, MYSQLI_ASSOC)) {$cart = $row["cart_id"];}
+  foreach($_POST as $book_quantity => $value)
+  {
+    if($value != 0)
+    {
     echo "<br>Post Parameter '$book_quantity' has '$value'";
-    //$list_of_books .= $book_quantity .";
-    }
+    $query1="select title from book where book_id ='$book_quantity'";
+    $result1 = mysqli_query($myconnection, $query1) or die ('Query failed: ' . mysql_error());
+            $hold_insert = "insert into hold(cart_id,book_id) values('$cart','$book_quantity')";
+            //Inserting books added to cart into hold
+            for($i =1; $i <= $value; $i++){$r1 = mysqli_query($myconnection, $hold_insert);}
+         /*if(mysqli_num_rows($result1)!=0)
+         {
+           while ($row = mysqli_fetch_array ($result1, MYSQLI_ASSOC)) 
+              {    
+                echo $row["title"];
+                echo "&nbsp;&nbsp;&nbsp;";
+                echo '<br>';
+                echo '<br>';
+              }
+          }*/
+      }
   }
-
-  echo "<form method = 'post' action = 'cart.php' >";
-  $query = "SELECT * FROM book where book_id in(select book_id from grad where order_id ='$cart_order_id'";
-  $result = mysqli_query($myconnection, $query) or die ('Query failed: ' . mysql_error());
-  $counter = 0;
-
-     // echo 'name'.$counter;
-
-  echo "<form method = 'post' action = 'confirm_purchase.php' >";
-  echo "<table>";
-
-
-  while($row = mysqli_fetch_array($result)){
-    $bookcounter = htmlspecialchars($row['book_id']);
-  
-    // to make it shows only author books add a query that checks author email to check if he is a customer
-    // and the book associated with his author_id and then add that to cart
-    
-    echo "<tr><td>". htmlspecialchars($row['book_id']). "</td>";
-    echo "<td>". htmlspecialchars($row['year']). "</td>";
-    echo "<td>". htmlspecialchars($row['genre']). "</td>";
-    echo "<td>". htmlspecialchars($row['title']). "</td>";
-    echo "<td>". htmlspecialchars($row['isbn']). "</td>";
-    echo "<td>". htmlspecialchars($row['book_condition']). "</td>";
-    echo "<td>". htmlspecialchars($row['price']). "</td>";
-    echo "<td>". htmlspecialchars($row['book_type']). "</td>";
-    echo "<td>". htmlspecialchars($row['total_rating']). "</td>";
-    echo "<td>Quantity: <select name='$bookcounter'>
-      <option value='0'></option>
-      <option value='1'>1</option>
-      <option value='2'>2</option>
-      <option value='3'>3</option>
-  <option value='4'>4</option>
-  <option value='5'>5</option>
-  </select></td>";
-    echo "</tr>";
-    
-
-}
-echo "</table>";
-echo "<br><br>";
-echo "<p align ='center'><input type='submit' value='Confirm Purchase'/></p>"; 
-echo "</form>";
+  echo "<form action ='ordered.php' action= 'post'>";
+  echo "<p align ='center'><input type='submit' value='Confirm Purchase'/></p>"; 
+  echo "</form>";
 ?>
+
 
 
   
